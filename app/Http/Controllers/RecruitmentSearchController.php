6d06035fbb6820b162e112d5884b25e528d2e295
Anusuya N <JMANDIGITAL\anusuya@anusuya.jmandigital.in>
2019-12-13 18:58:45 +0530

    Commit for Bug Fixes

--- app/Http/Controllers/RecruitmentSearchController.php
+++ app/Http/Controllers/RecruitmentSearchController.php
@@ -19,53 +19,56 @@ class RecruitmentSearchController extends Controller
     {
         if(isset($request->firm_id)){
 
-            if((isset($request->search_locations))||(isset($request->service_id))||(isset($request->recruitment_id))||(isset($request->size))||(isset($request->practice_area_id))||(isset($request->sector_id))){
-
-                $request->session()->put('firm_id','');
-            }else{
                 $request->session()->put('firm_id',$request->firm_id);
-
-            }
-    }else{
+                $request->session()->forget('location_id');
+                $request->session()->forget('service_id');
+                $request->session()->forget('recruitment_id');
+                $request->session()->forget('firm_size');
+                $request->session()->forget('practice_area_id');
+                $request->session()->forget('sector_id');
+          
+        }
+        else{
             $request->session()->forget('firm_id');
-        }
-        
-        if(isset($request->search_locations)){
-            $request->session()->put('location_id',$request->search_locations);
-        }else{
-            $request->session()->forget('location_id');
-        }
 
-        if(isset($request->service_id)){
-            $request->session()->put('service_id',$request->service_id);
-        }else{
-            $request->session()->forget('service_id');
+            if(isset($request->search_locations)){
+                $request->session()->put('location_id',$request->search_locations);
+            }else{
+                $request->session()->forget('location_id');
+            }
+    
+            if(isset($request->service_id)){
+                $request->session()->put('service_id',$request->service_id);
+            }else{
+                $request->session()->forget('service_id');
+            }
+    
+            if(isset($request->recruitment_id)){
+                $request->session()->put('recruitment_id',$request->recruitment_id);
+            }else{
+                $request->session()->forget('recruitment_id');
+            }
+    
+            if(isset($request->size)){
+                $request->session()->put('firm_size',$request->size);
+            }else{
+                $request->session()->forget('firm_size');
+            }
+    
+            if(isset($request->practice_area_id)){
+                $request->session()->put('practice_area_id',$request->practice_area_id);
+            }else{
+                $request->session()->forget('practice_area_id');
+            }
+    
+            if(isset($request->sector_id)){
+                $request->session()->put('sector_id',$request->sector_id);
+            }else{
+                $request->session()->forget('sector_id');
+            }
+    
         }
-
-        if(isset($request->recruitment_id)){
-            $request->session()->put('recruitment_id',$request->recruitment_id);
-        }else{
-            $request->session()->forget('recruitment_id');
-        }
-
-        if(isset($request->size)){
-            $request->session()->put('firm_size',$request->size);
-        }else{
-            $request->session()->forget('firm_size');
-        }
-
-        if(isset($request->practice_area_id)){
-            $request->session()->put('practice_area_id',$request->practice_area_id);
-        }else{
-            $request->session()->forget('practice_area_id');
-        }
-
-        if(isset($request->sector_id)){
-            $request->session()->put('sector_id',$request->sector_id);
-        }else{
-            $request->session()->forget('sector_id');
-        }
-
+       
         $firms = $this->filterFirm($request);
 
         return view('search-results', compact('firms'));
