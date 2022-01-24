6acde3b876a6b691e524d7321c1825e3c9acc098
Shirpi Anbarasan <shirpianbarasan@jmangroup.com>
2022-01-24 15:22:29 +0530

    Login Responsive completed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <div class="bg-signin">
     <div class="container d-flex align-items-center w-100 h-100 p-3 mx-auto signIn">
         <div class="row pt-9">
-            <div class="col-md-7">
+            <div class="col-md-7 d-mob-hide">
                
                 <h5 class="lh5 pb-3">Our goal is to provide law firm and in-house legal resourcing teams with greater knowledge and information about the legal recruitment market, to help improve efficiency, transparency and the services provided throughout the recruitment process.</h5>
 
@@ -23,11 +23,11 @@
                     <small class="text-muted">info@legalrecruitmentmadeclear.com</small>
                 </footer> -->
             </div>
-            <div class="col-md-5">
+            <div class="col-md-5 col-12">
                 <div class="row">
                     <div class="col-md-9 offset-md-3">
                         <div class="row">
-                            <div class="col-md-12 text-right">
+                            <div class="col-md-12 text-md-right text-center">
                                 <small> <img class="pb-5" src="../img/logo-header.png" alt="Recdirec"></small>
                             </div>
                         </div>
@@ -36,79 +36,77 @@
                                 <h4 class="text-white py-2 lh5">{{ __('Sign me in') }}</h4>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <form method="POST" action="{{ route('login') }}" id="login">
-                                    @csrf
-                                    @if(count($errors))
-                                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                        @foreach($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
+                        <div>
+                            <form method="POST" action="{{ route('login') }}" id="login">
+                                @csrf
+                                @if(count($errors))
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                @endif
+                                <div class="form-group">
+                                    <input id="email" type="email" class="form-control mb-1 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email Address">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input id="password" type="password" class="form-control mb-1 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                {{-- @if(\App\SiteConstants::CAPTCHA_SITE_KEY)
+                                <div class="form-group py-2">
+                                    <div class="g-recaptcha" name="g-recaptcha-response" data-sitekey="{!! \App\SiteConstants::CAPTCHA_SITE_KEY !!}"></div>
+
+                                    @if ($errors->has('g-recaptcha-response'))
+                                        <span class="invalid-feedback" style="display: block;">
+                                            <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+                                        </span>
                                     @endif
-                                    <div class="form-group">
-                                        <input id="email" type="email" class="form-control mb-1 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email Address">
-
-                                        @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                    <div class="form-group">
-                                        <input id="password" type="password" class="form-control mb-1 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
-
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                    {{-- @if(\App\SiteConstants::CAPTCHA_SITE_KEY)
-                                    <div class="form-group py-2">
-                                        <div class="g-recaptcha" name="g-recaptcha-response" data-sitekey="{!! \App\SiteConstants::CAPTCHA_SITE_KEY !!}"></div>
-
-                                        @if ($errors->has('g-recaptcha-response'))
-                                            <span class="invalid-feedback" style="display: block;">
-                                                <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
-                                            </span>
-                                        @endif
-                                    </div>
-                                    @endif --}}
-                                    <div class="form-group">
-                                        <button type="submit" class="btn py-2 w-100 signin-Button">
-                                            {{ __('Sign In') }}
-                                        </button>
-                                    </div>
-                                    <div class="form-group pt-1">
-                                        @if (Route::has('password.request'))
+                                </div>
+                                @endif --}}
+                                <div class="form-group">
+                                    <button type="submit" class="btn py-2 w-100 signin-Button">
+                                        {{ __('Sign In') }}
+                                    </button>
+                                </div>
+                                <div class="form-group pt-1">
+                                    @if (Route::has('password.request'))
+                                    <small>
+                                        <a class="text-signIn" href="{{ route('password.request') }}">
+                                            {{ __('Forgotten Password?') }}
+                                        </a>
+                                    </small>
+                                    @endif
+                                </div>
+                                <!--- <div class="row mb-0">
+                                    <div class="col-7">
                                         <small>
-                                            <a class="text-signIn" href="{{ route('password.request') }}">
-                                                {{ __('Forgotten Password?') }}
-                                            </a>
+                                            <a href="{{ url('/terms') }}" class="text-signIn">Terms & Conditions</a>
+                                            <span class="text-signIn"> | </span>
+                                            <a href="{{ url('/privacy') }}" class="text-signIn">Privacy Policy</a>
                                         </small>
-                                        @endif
                                     </div>
-                                    <!--- <div class="row mb-0">
-                                        <div class="col-7">
-                                            <small>
-                                                <a href="{{ url('/terms') }}" class="text-signIn">Terms & Conditions</a>
-                                                <span class="text-signIn"> | </span>
-                                                <a href="{{ url('/privacy') }}" class="text-signIn">Privacy Policy</a>
-                                            </small>
-                                        </div>
-                                        <div class="col-5 text-right">
-                                            <i class="fa fa-instagram" aria-hidden="true"></i>
-                                            <i class="fa fa-twitter px-2" aria-hidden="true"></i>
-                                            <i class="fa fa-facebook" aria-hidden="true"></i>
-                                        </div>
-                                    </div>  --->
-                                </form>
-                            </div>
+                                    <div class="col-5 text-right">
+                                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                                        <i class="fa fa-twitter px-2" aria-hidden="true"></i>
+                                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                                    </div>
+                                </div>  --->
+                            </form>
                         </div>
                     </div>
                 </div>
